6deeeb65e7d274c0cfef65e204438299866a1391
MultiMouse <olmos.luisd@outlook.com>
2024-01-28 21:42:27 -0600

    added queries.js, set up get_me

---
+++ client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_GET_ME = gql`
+  query me($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
